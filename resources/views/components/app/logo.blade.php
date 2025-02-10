68d138d93ef0365f6b84f0804219729a49a4b38a
Daniel Lopes <dalopes@id-logistics.com>
2025-02-10 12:08:32 -0300

    change logo

--- resources/views/components/app/logo.blade.php
+++ resources/views/components/app/logo.blade.php
@@ -3,16 +3,15 @@
 @endphp
 
 @if (Filament::getTenant() != null)
-    <div class="flex items-center space-x-2">
-        @if (Filament::getTenant()->code == 'cddnpc' ||
-                Filament::getTenant()->code == 'fbdnpc' ||
-                Filament::getTenant()->code == 'fbdlnpc' ||
-                Filament::getTenant()->code == 'cddnpa')
-            <img src="{{ asset('/tenants/danone.svg') }}" alt="Logo" class="h-12 dark:bg-white p-1 rounded-lg">
-        @endif
+    <div class="flex items-center">
+        <x-filament::icon
+            class="h-10 w-10 text-gray-500 dark:text-gray-400"
+            icon="heroicon-o-book-open" />
     </div>
 @else
-    <div class="flex items-center space-x-2">
-        <img src="{{ asset('/logo.svg') }}" alt="Logo" class="h-10">
+    <div>
+        <x-filament::icon
+            class="h-10 w-10 text-gray-500 dark:text-gray-400"
+            icon="heroicon-o-book-open" />
     </div>
 @endif
